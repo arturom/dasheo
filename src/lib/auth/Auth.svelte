1aad983c5ce8b2b3ef14abbb55f15c1d44069546
Arturo <arturomejiac@gmail.com>
2025-03-23 08:10:49 -0400

    Hide auth form after log in

--- src/lib/auth/Auth.svelte
+++ src/lib/auth/Auth.svelte
@@ -1,107 +1,22 @@
 <script lang="ts">
-    import { createWithBasicAuth, createWithHeader, type StreamsClient } from "../streams/client";
+    import type { StreamsClient } from "../streams/client";
+    import Form from "./Form.svelte";
 
     interface Props {
-        onConnect: (c: StreamsClient) => void;
+        onConnect: (client: StreamsClient) => void;
     }
 
     const { onConnect }: Props = $props();
 
-    let baseUrl: string = $state('nuxeo/api/v1');
-    let method: string = $state('basic');
-    let username: string = $state('Administrator');
-    let password: string = $state('Administrator');
-    let headerName: string = $state('');
-    let headerValue: string = $state('');
+    let isConnected: boolean | null = $state(false);
 
-    async function authenticate(e: SubmitEvent) {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        if (method === 'basic') {
-            basicAuth();
-        } else if (method === 'custom') {
-            customAuth();
-        }
-    }
-
-    async function basicAuth() {
-        const client = createWithBasicAuth(baseUrl, username, password);
+    function connected(client: StreamsClient) {
         onConnect(client);
-    }
-
-    async function customAuth() {
-        const client = createWithHeader(baseUrl, headerName, headerValue);
-        onConnect(client);
+        isConnected = true;
     }
 
 </script>
 
-<form onsubmit={authenticate}>
-    <h3>Authenticate</h3>
-    <div class="input">
-        <label for="base-url">Base URL</label>
-        <input id="base-url" type="text" bind:value={baseUrl} />
-    </div>
-    <div class="radio">
-        <input type="radio" bind:group={method} value="basic" id="opt-auth-basic" />
-        <label for="opt-auth-basic">Basic authentication</label>
-        <input type="radio" bind:group={method} value="custom" id="opt-auth-custom" />
-        <label for="opt-auth-custom">Custom authentication</label>
-    </div>
-
-    {#if method === 'basic'}
-        <div class="input">
-            <label for="username">Username</label>
-            <input id="username" type="text" bind:value={username} />
-        </div>
-        <div class="input">
-            <label for="password">Password</label>
-            <input id="password" type="password" bind:value={password} />
-        </div>
-    {:else if method === 'custom'}
-        <div class="input">
-            <label for="header-name">Header Name</label>
-            <input id="header-name" type="text" bind:value={headerName} />
-        </div>
-        <div class="input">
-            <label for="header-value">Header Value</label>
-            <input id="header-value" type="password" bind:value={headerValue} />
-        </div>
-    {/if}
-
-    <button>Connect</button>
-</form>
-
-<style>
-  form {
-    display: flex;
-    flex-direction: column;
-    max-width: 30em;
-    margin: 0 auto;
-    padding: 1em;
-    gap: 1em
-  }
-
-  form .radio {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    gap: .2em;
-  }
-
-  form .input {
-    display: flex;
-    flex-direction: column;
-    gap: 0;
-  }
-
-  form .input label {
-    font-size: .8rem;
-  }
-
-  input, button {
-    padding: .4em;
-  }
-
-
-</style>
+{#if !isConnected}
+    <Form onConnect={connected}/>
+{/if}
